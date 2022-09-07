51bf951fc338cb8c1d3f4e797376111a4b91e72e
dckrOff <azamatotaboev7621@gmail.com>
2022-09-07 17:12:47 +0500

    first commit

---
+++ app/src/main/java/com/a1tech/calculator_android/MainActivity.java
@@ -0,0 +1,20 @@
+package com.a1tech.calculator_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+
+
+    }
+}
